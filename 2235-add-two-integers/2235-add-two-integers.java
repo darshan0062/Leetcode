8cf9b1cf2ef3a6fdbc25cce1b2c2dc1ebce2e537
M Darshan <darshan6362593330@gmail.com>
2026-08-06 00:28:18 +0530

    Time: 0 ms (100%), Space: 42.2 MB (56.02%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+     }
+}
+// class main{
+//     public static void main(String[]args){
+//         Solution obj = new Solution();
+//         int result= obj.sum(12,5);
+//         System.out.println(result); 
+//     }
+// }
